d9960872bbf6f45ee25c36101b1ba73c6052d1a1
keyurgovanitops <keyurgovani.tops@gmail.com>
2026-01-28 18:15:37 +0530

    Add files via upload

--- array.js
+++ array.js
@@ -47,9 +47,8 @@ arr1[2] = 3;
 // unshift
 // push
 // pop
-// length
 
-let arr4 = [11,12,13,14,15];
+
 // 0,1,
 
 // console.log(arr4); // 11,12,13,14,15
@@ -64,14 +63,67 @@ let arr4 = [11,12,13,14,15];
 
 
 // map :-  it does return new array, it doest not change the original array
-console.log(arr4); // 15
+// console.log(arr4); // 15
 
-let u2 = arr4.map((value,index)=>{
-    return value*2;// jo ki mujhe mere naye array ke andar chahiye
-})
+// let u2 = arr4.map((value,index)=>{
+//     return value*2;// jo ki mujhe mere naye array ke andar chahiye
+// })
 
-console.log(u2);
-console.log(arr4); // 0-11 
+// console.log(u2);
+// console.log(arr4); // 0-11 
+
+ 
+
+//  filter :-  it does return new array, it doest not change the original array, used for filtering the data in array, it checks the condition on all the elements 
+
+// let u3 = arr4.filter(value=> value > 13);
+// let u3 = arr4.filter(value=>{
+//     return value > 13;
+// })
+// function ab(value){
+//     return value> 13
+// }
+// let u3 = arr4.filter(ab);
+
+// let arr4 = [11,12,13,16,14,15,16,11];
+
+// console.log('this is arr4', arr4); 
+
+//  find :-  it does return new array, it doest not change the original array, used for finding a single instance in element.
+
+// let u4 = arr4.find((value)=>{
+//     return value===22;
+// })
+
+
+// reduce:- it returns a value, it doest not change the original array, used for finding a single value of an array,
+// example :- average of array, 
+
+// let arr4 = [1,1,1,1,35];
+
+// function addition(total, current){
+//     console.log('total: ' + total, 'current: ' + current);
+//     return total + current;
+// }
+// let u5 = arr4.reduce(addition);
+
+// console.log(u5);
+// console.log('this is arr4', arr4);
+
+// shift: 
+
+let arr5 = [1,2,3,4];
+// arr5.shift();
+// arr5.unshift(20);
+// arr5.push(25);
+// arr5.pop();
+
+console.log(arr5);
+
+
+
+
+
 
 
 
